c40c29c56fce3a50974bb6a130e55dbca33bcaab
vladimirovicp <vladimirovicp70@gmail.com>
2023-04-25 15:02:40 +0400

    add: 004 Создание компонентов

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Quiz from './components/Quiz';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Quiz />
+  </React.StrictMode>,
+    document.getElementById("root")
 );
